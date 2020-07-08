44ddc3f6d518b215b4f0644140b7a19fc7d96ef2
iliass <iliasspro1@gmail.com>
2020-07-08 14:01:00 +0100

    second commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('/client','ClientController');
+Route::resource('/pisserie','PisserieController');
+Route::resource('/transaction','TransactionController');
